21d8eda4c261042defc131ef899a3be1a260b897
Florent <contact@making.codes>
2023-09-28 15:01:09 +0200

    feat(WORKFLOW): update test fonctionnel

--- tests/fonctionnel.js
+++ tests/fonctionnel.js
@@ -1,6 +1,7 @@
 const { Builder } = require('selenium-webdriver');
 const chrome      = require('selenium-webdriver/chrome');
 const path        = require('path');
+const fs          = require('fs');
 
 /**
  * Initialize WebDriver for Electron
@@ -8,21 +9,25 @@ const path        = require('path');
  * @return {Object} - Selenium WebDriver object
  */
 function initWebDriver(appPath) {
+  const distPath      = "/node_modules/electron/dist"
   const chromeOptions = new chrome.Options();
 
   // Set ChromeDriver binary path
   process.env['PATH'] += path.delimiter + path.join(appPath, './node_modules/chromedriver/lib/chromedriver');
   
-  let epath = path.join(appPath, '/node_modules/electron/dist/Electron.app/Contents/MacOS/Electron')
-  console.log(epath)
-  chromeOptions.setChromeBinaryPath(epath);
+  // Mac    dist/Electron.app/Contents/MacOS/Electron
+  // Window dist/electron.exe
+  // Linux  dist/electron
+  let electronPath = fs.existsSync(appPath + distPath + '/Electron.app/Contents/MacOS/Electron') 
+                      ? appPath + distPath + '/Electron.app/Contents/MacOS/Electron'
+                      : appPath + distPath
+
+  chromeOptions.setChromeBinaryPath(electronPath);
 
   // Set app path as a Chrome option
   chromeOptions.addArguments(`app=${appPath}`);
   chromeOptions.addArguments('--disable-web-security');
   chromeOptions.addArguments('--auto-open-devtools-for-tabs');
-
-  console.log(appPath)
 
   // Initialize WebDriver
   return new Builder()
@@ -42,8 +47,6 @@ function initWebDriver(appPath) {
     console.error('An error occurred:', error);
   } finally {
     // Quit the WebDriver session
-    setTimeout( async () => {
-       await driver.quit();
-    }, 60 * 1000)
+    await driver.quit();
   }
 })();
